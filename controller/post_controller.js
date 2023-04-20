e556210936d03e9a42f07931cce7a6a4b8971953
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-04-20 09:39:50 +0530

    Delete a post(authorized)

--- controller/post_controller.js
+++ controller/post_controller.js
@@ -1,5 +1,6 @@
 // import the PostSchema in controller
  const Post = require('../models/post_schema');
+ const Comment = require('../models/comments_schema'); // this is for the delete the post as well as comment
 
  module.exports.create = function(req,res){
     // field Set
@@ -15,3 +16,23 @@
 
     });
  }
+
+//  deleting the a post (authorirzed);
+
+module.exports.destroy = function(req,res){
+    // yaha sabse pahle requrested id se us post ko khojenge
+    Post.findById(req.params.id, function(err,post){
+         // .id means converting the object id into string
+        if(post.user == req.user.id){ 
+            // authorized check kya user sign in hain wo req.user.id se match hota hain
+            post.remove();
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back')
+            });
+        }
+        else{
+            return res.redirect('back')
+        }
+
+    });
+}
