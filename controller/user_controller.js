b879239f7087242672bd6f31c61835fb94fb8051
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-04-25 04:15:38 +0530

    Updating the user profile Links and bio data

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -31,6 +31,18 @@ module.exports.profile = function(req,res){
     
 };
 
+// update the user profile name and others
+module.exports.update = function(req,res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body,function(err,post){
+            return res.redirect('back');
+        })
+    }
+    else{
+        res.status(401).send('Unauthorized')
+    }
+}
+
 // render the sign up page
 module.exports.signUp = function(req,res){
     if(req.isAuthenticated()){
